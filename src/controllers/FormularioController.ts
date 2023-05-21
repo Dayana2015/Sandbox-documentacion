76c51e1347a0150159538965aaeef8ba0a281630
Brillant-Minds-Grupo-5 <mosanz02@gmail.com>
2023-05-21 09:48:03 -0500

    Actualizacion prisma

---
+++ src/controllers/FormularioController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import * as jsonSchema from "../json-schema.json"
+
+
+type JsonSchema  = Record< string, any>
+
+
+
+class Formulario {
+
+    obtenerDefinicion(req: Request, res: Response) {
+        //1 ectraer el nombre del formulario
+        const { formulario } = req.params
+
+        const miEsquema: JsonSchema=jsonSchema
+
+        if (miEsquema.definitions[formulario]) {
+            res.json(miEsquema.definitions[formulario])
+        } else {
+            res.status(400)
+            res.json({ error: `no se encontro el formulario ${formulario}` })
+        }
+    }
+}
+export default Formulario
